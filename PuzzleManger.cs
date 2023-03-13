65aa1a4c3d09af904331f98fff94eae30cb24a9a
HSULW <113079390+HSULW@users.noreply.github.com>
2023-03-14 03:34:19 +0800

    Create PuzzleManger.cs

---
+++ PuzzleManger.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PuzzleManger : MonoBehaviour
+{
+    public static PuzzleManger Instance;
+
+    [SerializeField]
+    RawImage test;
+
+    private void Awake()
+    {
+        Instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadTexture();
+    }
+
+    void LoadTexture()
+    {
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+        openFileDialog.Filter = "JPG|*.jpg|PNG_image|*.png";
+        if (openFileDialog.ShowDialog() == DialogResult. OK) 
+        {
+            string texPath = openFileDialog. FileName;
+            Texture2D tex2d = new Texture2D(1, 1);
+            byte[] texByte = File.ReadAllBytes(texPath);
+            tex2d. LoadImage(texByte);
+            test. texture = tex2d;
+        }
+    }
+}
